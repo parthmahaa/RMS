7b8f4cf958d8b6a2e85b091d56286e270b584dd3
Parth Maha <parthmaha9@gmail.com>
2026-01-04 20:41:29 +0530

    screening stage implementation

--- backend/src/main/java/com/rms/dto/jobs/JobStatusDto.java
+++ backend/src/main/java/com/rms/dto/jobs/JobStatusDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class JobCloseDto {
+public class JobStatusDto {
     private String closeReason;
     private List<Long> selectedCandidateIds;
 }
